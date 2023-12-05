e0aedab2bcc7a72704acde38012e21ad2ebf6706
anrit <anri.tabuev@gmail.com>
2023-12-05 20:01:55 +0300

    dragon draw

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from PyQt5.QtCore import Qt, QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from sys import argv
 import requests
 app = Flask(__name__)
+from PIL import Image, ImageDraw
+import io
 
+def dragon(x1, y1, x2, y2, depth):
+    def paint(draw, x1, y1, x2, y2, k):
+        if k == 0:
+            draw.line([x1, y1, x2, y2], fill='white')
+        else:
+            tx = (x1 + x2) // 2 + (y2 - y1) // 2
+            ty = (y1 + y2) // 2 - (x2 - x1) // 2
+            paint(draw, x2, y2, tx, ty, k - 1)
+            paint(draw, x1, y1, tx, ty, k - 1)
+
+    img = Image.new('RGB', (750, 750), color='black')
+    draw = ImageDraw.Draw(img)
+    paint(draw, x1, y1, x2, y2, depth)
+
+    return img
+@app.route('/draw_dragon')
+def draw_dragon():
+    img = dragon(50, 350, 650, 350, 14)
+
+    buf = io.BytesIO()
+    img.save(buf, format='PNG')
+    buf.seek(0)
+
+    return send_file(buf, mimetype='image/png')
 
 @app.route('/joke')
 def joke():
